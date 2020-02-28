78ccb7a8db350014ab663a4a64c30247180f9764
Brendan <ngoranbren@gmail.com>
2020-02-28 10:14:19 -0600

    made a substring program

---
+++ CPU-github.csproj/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a text in a Line");
+
+            string nstring = Console.ReadLine();
+
+
+            string[] estring = nstring.Split(" ");
+
+
+            
+            for (int i = 0; i < estring.Length; i++)
+            {    //searching through e string array and printing the first index
+                // and ignoring every other index after.
+                Console.Write(estring[i].Substring(0, 1));
+
+
+            }
+
+
+        }
+    }
+}
