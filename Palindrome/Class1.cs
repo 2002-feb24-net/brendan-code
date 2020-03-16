cccf267cbde9019e6cbd6f4014f41e6b958f5d02
Brendan <ngoranbren@gmail.com>
2020-03-16 12:36:51 -0500

    Palindrome challenge

---
+++ Palindrome/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            string string1, rev;
+            string1 = "racecaR";
+            char[] ch = string1.ToCharArray();
+            Array.Reverse(ch);
+            rev = new string(ch);
+            bool b = string1.Equals(rev, StringComparison.OrdinalIgnoreCase);
+            if (b == true)
+            {
+                Console.WriteLine(" " + string1 + " is a Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine(" " + string1 + " is not a Palindrome!");
+            }
+            Console.Read();
+
+            string string2;
+            string2 = "123abccba123";
+            char[] ch = string2.ToCharArray();
+            Array.Reverse(ch);
+            rev = new string(ch);
+            string2.Equals(rev, StringComparison.OrdinalIgnoreCase);
+            if (b == true)
+            {
+                Console.WriteLine("" + string2 + "is a Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine(" " + string2 + "is not a Palindrome!");
+            }
+            Console.Read();
+
+        }
+            
+    }
+}
